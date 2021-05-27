1fbf51e639f4c24a117258301527091b87000ffe
sxiong0519 <sxiong0519@gmail.com>
2021-05-27 14:29:28 -0500

    adding .push notes

--- notes.js
+++ notes.js
@@ -13,6 +13,17 @@ const notes = [
         feeling: "Very complicated process, but extremely helpful when using with team. Once I got the hang of the different commands, it was not all that bad. Okay feeling currently",
         timeSpent: 2000
     }
+
 ]
+
+const pushNote = {
+    id: 3,
+    subject: ".push in array",
+    date: "May 27, 2021", 
+    feeling: "easy concept once I knew what it does. Extremely helpful as I have noticed. Hope to use it more!! Hope it doesn't get too complicated.",
+    timeSpent: 30
+}
+
+notes.push(pushNote)
 
 console.log(notes)
